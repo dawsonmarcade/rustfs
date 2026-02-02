40131d8d6c6e059546338566adb75c4993cacac0
Dawson <dawsonmarcade@gmail.com>
2026-02-01 22:31:52 -0600

    Built out first backend service with rust

---
+++ backend/src/main.rs
@@ -0,0 +1,45 @@
+#[macro_use] extern crate rocket;;
+
+use rocket::fs::{ Deserialize, Serialize, json::Json };
+usse rocket:: {State, response::status::Custom, http::Status};
+use tokio_postgres::{ Client, NoTls};
+use rocket_cors::{ CorsOptions, AllowedOrigins};
+
+#[derive(Serialize, Deserialize, Clone)]
+struct User {
+    id: Option<i32,
+    name: String,
+    email: String,
+}
+
+#[post("/api/users", data = "<user>)]
+asnyc fn add_user(
+    conn: &State<Client>,,
+    user: Json<User>,
+) -> Result<Custom<Json<User>>, Custom<String>> {
+    execute_query(
+        conn,
+        "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id",
+        &[&user.name, &user.email],
+    ).await?;
+    get_users(conn).await
+}
+
+
+#[get("/api/users")]
+asnyc fn get_users(
+    conn: &State<Client>) -> Result<Custom<Json<Vec<User>>>, Custom<String>> {
+    get_users_from_db(conn).await
+}
+
+
+async fn execute_query(
+    client: &Client,
+    query: &str,
+    params: &[&(dyn tokio_postgres::types::ToSql + Sync)],
+) -> Result<u64, Custom<String>> {
+    client &Client 
+        .execute(statement: query, params)
+        .await
+        .map_err(|e| Custom(Status::InternalServerError, e.to_string()))
+}
